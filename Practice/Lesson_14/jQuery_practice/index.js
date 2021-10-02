ae02c5ce44b6e00db62b147364f85107cb17de2d
Wei Dong <wei.dong@i-focus.co.jp>
2021-10-03 01:11:30 +0900

    Lesson_18 finished

---
+++ Practice/Lesson_14/jQuery_practice/index.js
@@ -0,0 +1,5 @@
+$("button").on("click",function(){
+  // $("h1").animate({opacity:0.5});
+  // $("h1").animate({margin:"300px"});
+  $("h1").slideUp().slideDown().animate({opacity:0.5});
+});
---
+++ Practice/Lesson_18/node_js_practice/index.js
@@ -0,0 +1,14 @@
+// jshint esversion:6
+
+// const superheroes = require('superheroes');
+// for(i=0;i<10;i++){
+//   var mySuperheroName = superheroes.random();
+//   console.log(mySuperheroName);
+// }
+
+
+const supervillains = require('supervillains');
+
+for(i=0;i<10;i++){
+  console.log(supervillains.random());
+}
